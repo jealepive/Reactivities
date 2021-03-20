6b23acb37f0082691a6575a933d0bfd057f46f2f
Jean Alejandro Pico <jealepive@hotmail.com>
2021-03-20 11:46:37 -0500

    Instalacion de Entity Framework + SQLite DB + Controladores de API para obtener informacion de actividades

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Se crea tabla en la DB con el mismo nombre 'Activities'
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
