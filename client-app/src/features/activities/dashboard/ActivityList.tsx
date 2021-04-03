2ff26711cd519e8f6d61baed7b27a1d59c2db257
Jean Alejandro Pico <jealepive@hotmail.com>
2021-04-03 12:58:19 -0500

    Operaciones CRUD desde React, ahora persisten la informacion/cambios en la base de datos

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,43 +1,52 @@
-import React from "react";
-import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import React, { SyntheticEvent, useState } from 'react';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
 
 interface Props {
-	activities: Activity[];
-	selectActivity: (id: string) => void;
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({ activities, selectActivity }: Props) {
-	return (
-		<Segment>
-			<Item.Group divided>
-				{activities.map((activity) => (
-					<Item key={activity.id}>
-						<Item.Content>
-							<Item.Header as="a">{activity.title}</Item.Header>
-							<Item.Meta>{activity.date}</Item.Meta>
-							<Item.Description>
-								<div>{activity.description}</div>
-								<div>
-									{activity.city}, {activity.venue}
-								</div>
-							</Item.Description>
-							<Item.Extra>
-								<Button
-									onClick={() => selectActivity(activity.id)}
-									floated="right"
-									content="View"
-									color="blue"
-								></Button>
-								<Label
-									basic
-									content={activity.category}
-								></Label>
-							</Item.Extra>
-						</Item.Content>
-					</Item>
-				))}
-			</Item.Group>
-		</Segment>
-	);
+export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(event: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(event.currentTarget.name);
+        deleteActivity(id);
+    }
+
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activities.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{activity.description}</div>
+                                <div>
+                                    {activity.city}, {activity.venue}
+                                </div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue'></Button>
+                                <Button
+                                    name={activity.id}
+                                    loading={submitting && target === activity.id}
+                                    onClick={event => handleActivityDelete(event, activity.id)}
+                                    floated='right'
+                                    content='Delete'
+                                    color='red'
+                                ></Button>
+                                <Label basic content={activity.category}></Label>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    );
 }
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,91 +1,53 @@
-import React, { ChangeEvent, useState } from "react";
-import { Button, Form, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import React, { ChangeEvent, useState } from 'react';
+import { Button, Form, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
 
 interface Props {
-	activity: Activity | undefined;
-	closeForm: () => void;
+    activity: Activity | undefined;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({
-	activity: selectedActivity,
-	closeForm,
-}: Props) {
-	const initialState = selectedActivity ?? {
-		id: "",
-		title: "",
-		category: "",
-		description: "",
-		date: "",
-		city: "",
-		venue: "",
-	};
+export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, submitting }: Props) {
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: '',
+    };
 
-	const [activity, setActivity] = useState(initialState);
+    const [activity, setActivity] = useState(initialState);
 
-	function handleSubmit() {
-		console.log(activity);
-	}
+    function handleSubmit() {
+        createOrEdit(activity);
+    }
 
-	function handleInputChange(
-		event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-	) {
-		const { name, value } = event.target;
-		setActivity({ ...activity, [name]: value });
-	}
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const { name, value } = event.target;
+        setActivity({ ...activity, [name]: value });
+    }
 
-	return (
-		<Segment clearing>
-			<Form onSubmit={handleSubmit} autoComplet="off">
-				<Form.Input
-					onChange={handleInputChange}
-					placeholder="Title"
-					value={activity?.title}
-					name="title"
-				></Form.Input>
-				<Form.TextArea
-					onChange={handleInputChange}
-					placeholder="Description"
-					value={activity?.description}
-					name="description"
-				></Form.TextArea>
-				<Form.Input
-					onChange={handleInputChange}
-					placeholder="Category"
-					value={activity?.category}
-					name="category"
-				></Form.Input>
-				<Form.Input
-					onChange={handleInputChange}
-					placeholder="Date"
-					value={activity?.date}
-					name="date"
-				></Form.Input>
-				<Form.Input
-					onChange={handleInputChange}
-					placeholder="City"
-					value={activity?.city}
-					name="city"
-				></Form.Input>
-				<Form.Input
-					onChange={handleInputChange}
-					placeholder="Venue"
-					value={activity?.venue}
-					name="venue"
-				></Form.Input>
-				<Button
-					floated="right"
-					positive
-					type="submit"
-					content="Submit"
-				></Button>
-				<Button
-					onClick={closeForm}
-					floated="right"
-					type="button"
-					content="Cancel"
-				></Button>
-			</Form>
-		</Segment>
-	);
+    return (
+        <Segment clearing>
+            <Form onSubmit={handleSubmit} autoComplet='off'>
+                <Form.Input onChange={handleInputChange} placeholder='Title' value={activity?.title} name='title'></Form.Input>
+                <Form.TextArea
+                    onChange={handleInputChange}
+                    placeholder='Description'
+                    value={activity?.description}
+                    name='description'
+                ></Form.TextArea>
+                <Form.Input onChange={handleInputChange} placeholder='Category' value={activity?.category} name='category'></Form.Input>
+                <Form.Input onChange={handleInputChange} type='date' placeholder='Date' value={activity?.date} name='date'></Form.Input>
+                <Form.Input onChange={handleInputChange} placeholder='City' value={activity?.city} name='city'></Form.Input>
+                <Form.Input onChange={handleInputChange} placeholder='Venue' value={activity?.venue} name='venue'></Form.Input>
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit'></Button>
+                <Button onClick={closeForm} floated='right' type='button' content='Cancel'></Button>
+            </Form>
+        </Segment>
+    );
 }
