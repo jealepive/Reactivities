88448d16b387a3dc01818f0224ea32dc864bfb6e
Jean Alejandro Pico <jealepive@hotmail.com>
2021-04-05 08:34:49 -0500

    Se agrega navegacion y routing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,22 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
-    const { activityStore } = useStore();
-
-    useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore]);
-
-    if (activityStore.loadingInitial) {
-        return <LoadingComponent content='Loading app...'></LoadingComponent>;
-    }
-
     return (
         <Fragment>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-                <ActivityDashboard />
+                <Route exact path='/' component={HomePage}></Route>
+                <Route path='/activities' component={ActivityDashboard}></Route>
+                <Route path='/activities/:id' component={ActivityDetails}></Route>
+                <Route path='/create-activity' component={ActivityForm}></Route>
             </Container>
         </Fragment>
     );
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
 import { useStore } from '../stores/store';
 
@@ -8,13 +9,13 @@ export default function NavBar() {
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' exact header>
                     <img src='/assets/logo.png' alt='logo' style={{ marginRight: '10px' }}></img>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name='Activities'></Menu.Item>
+                <Menu.Item as={NavLink} to='/activities' name='Activities'></Menu.Item>
                 <Menu.Item>
-                    <Button onClick={() => activityStore.openForm()} positive content='Create Activity'></Button>
+                    <Button as={NavLink} to='/create-activity' positive content='Create Activity'></Button>
                 </Menu.Item>
             </Container>
         </Menu>
